a9f721602eedfa289d0925244f910264b19cda86
huangweijie <949801851@qq.com>
2020-12-27 19:24:06 +0800

    使用sass处理器

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import addNumbers from './add'
 import minusNumbers from './minus'
 import './index.css'
 import './index.less'
+import './index.scss'
 
 console.log(addNumbers(1,1));
 console.log(minusNumbers(1,1));
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
                     'css-loader',
                     'less-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     }
