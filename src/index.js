b34aaecc69af7ec5d5b6591736f09028adbfac9e
huangweijie <949801851@qq.com>
2020-12-27 18:56:33 +0800

    使用css less 加载器，处理样式

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import addNumbers from './add'
 import minusNumbers from './minus'
+import './index.css'
+import './index.less'
 
 console.log(addNumbers(1,1));
 console.log(minusNumbers(1,1));
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,25 @@ module.exports = {
   output: {
       path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
-  }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    }
+        
 };
