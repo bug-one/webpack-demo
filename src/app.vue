bfb161ac63f3ab370355cafad188684f76fb28be
huangweijie <949801851@qq.com>
2020-12-29 10:03:49 +0800

    配置vue路由系统，实现单页面切换

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>vue</div>
+  <div>
+      <router-link to="/home">主页</router-link>
+      <router-link to="/login">登录页</router-link>
+      <router-view></router-view>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import app from './app.vue'
+import VueRouter from 'vue-router'
+import Home from './views/home.vue'
+import Login from './views/login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/home',
+        component:Home
+    },
+    {
+        path: '/login',
+        component:Login
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
 
 new Vue({
     el: '#app',
+    router,
     render(h) {
         return h(app)
     }
